306d66ca8fd891c9ed6ae15ebc85423438c6f459
Martin Zembaty <m.zembaty@posteo.de>
2020-01-10 04:13:34 +0100

    lower image quality

---
+++ src/components/mock-data/ingredient-season.js
@@ -0,0 +1,128 @@
+import apple from "../img/fruits/apple.jpg";
+import blueberrys from "../img/fruits/blueberrys.jpg";
+import cherries from "../img/fruits/cherries.jpg";
+import blackberry from "../img/fruits/blackberry.jpg";
+import currants from "../img/fruits/currants.jpg";
+import kiwi from "../img/fruits/kiwi.jpg";
+import raspberry from "../img/fruits/raspberry.jpg";
+import strawberry from "../img/fruits/strawberry.jpg";
+import cabageRed from "../img/vegetables/cabage-red.jpg";
+import carrots from "../img/vegetables/carrots.jpg";
+import cauliflower from "../img/vegetables/cauliflower.jpg";
+import corn from "../img/vegetables/corn.jpg";
+import lettuce from "../img/vegetables/lettuce.jpg";
+import mushrooms from "../img/vegetables/mushrooms.jpg";
+import paprikaGreen from "../img/vegetables/paprika-green.jpg";
+import potatoes from "../img/vegetables/potatoes.jpg";
+
+const farmProducts = [
+  {
+    img: apple,
+    title: "Apfel",
+    author: "Martin",
+    type: "fruit",
+    seasonMainGer: [1, 2, 3, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
+    img: blackberry,
+    title: "Brombeere",
+    author: "Martin",
+    type: "fruit",
+    seasonMainGer: []
+  },
+  {
+    img: blueberrys,
+    title: "Blaubeere",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: []
+  },
+  {
+    img: cherries,
+    title: "Kirsche",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: [6, 7]
+  },
+  {
+    img: currants,
+    title: "Johannisbeere",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: [5, 6, 7, 8]
+  },
+  {
+    img: kiwi,
+    title: "Kiwi",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: []
+  },
+  {
+    img: raspberry,
+    title: "Himbeere",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: [6, 7, 8, 9]
+  },
+  {
+    img: strawberry,
+    title: "Erdbeere",
+    author: "Isabel",
+    type: "fruit",
+    seasonMainGer: [5, 6, 7, 8, 9]
+  },
+  {
+    img: cabageRed,
+    title: "Rotkohl",
+    author: "Martin",
+    type: "vegetable",
+    seasonMainGer: [7, 8, 9, 10, 11]
+  },
+  {
+    img: carrots,
+    title: "Karrote",
+    author: "Martin",
+    type: "vegetable",
+    seasonMainGer: [5, 6, 7, 8, 9, 10, 11]
+  },
+  {
+    img: corn,
+    title: "Mais",
+    author: "Isabel",
+    type: "vegetable",
+    seasonMainGer: []
+  },
+  {
+    img: cauliflower,
+    title: "Blumenkohl",
+    author: "Isabel",
+    type: "vegetable"
+  },
+  {
+    img: lettuce,
+    title: "Salat",
+    author: "Isabel",
+    type: "vegetable"
+  },
+  {
+    img: mushrooms,
+    title: "Champignons",
+    author: "Isabel",
+    type: "vegetable"
+  },
+  {
+    img: paprikaGreen,
+    title: "Paprika",
+    author: "Isabel",
+    type: "vegetable"
+  },
+  {
+    img: potatoes,
+    title: "Kartoffel",
+    author: "Isabel",
+    type: "vegetable"
+  }
+];
+
+export default farmProducts;
